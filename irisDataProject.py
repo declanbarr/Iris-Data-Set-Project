76320c82ce7f33020379869d336cd8bcc891cff7
Declan Barr <deckybarr@googlemail.com>
2018-03-31 21:37:20 +0100

    code added to calculate mean of first axis

--- irisDataProject.py
+++ irisDataProject.py
@@ -1,8 +1,9 @@
 # Declan Barr 31 Mar 2018
 # Iris Flower Data Set Project for Programming and Scripting
-# Using numpy to read csv files https://www.dataquest.io/blog/numpy-cheat-sheet/
+# Using numpy to read csv files and determine mean
+# https://www.dataquest.io/blog/numpy-cheat-sheet/
 
 import numpy as np
 dataSet = np.genfromtxt('data/iris.csv',delimiter=',')
 
-print(dataSet)
+np.mean(dataSet, axis=0)
