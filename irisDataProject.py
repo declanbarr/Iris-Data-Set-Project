169d3ca297126ee5b5009778c3d3ca8f18705321
Declan Barr <deckybarr@googlemail.com>
2018-03-31 21:00:12 +0100

    Numpy code added

--- irisDataProject.py
+++ irisDataProject.py
@@ -1,2 +1,8 @@
 # Declan Barr 31 Mar 2018
 # Iris Flower Data Set Project for Programming and Scripting
+# Using numpy to read csv files https://www.dataquest.io/blog/numpy-cheat-sheet/
+
+import numpy as np
+dataSet = np.genfromtxt('file.csv',delimiter=',')
+
+print(dataSet)
