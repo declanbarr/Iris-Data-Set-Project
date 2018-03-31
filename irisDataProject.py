0711077e64a41c8035ade567c2f554bf5040aec0
Declan Barr <deckybarr@googlemail.com>
2018-03-31 23:51:37 +0100

    Added array for headings. Used a concatenate function from pandas

--- irisDataProject.py
+++ irisDataProject.py
@@ -3,11 +3,16 @@
 # Using numpy to read csv files and determine mean
 # https://www.dataquest.io/blog/numpy-cheat-sheet/
 # Using pandas to read csv files https://www.kaggle.com/ashokdavas/iris-data-analysis-pandas-numpy
+# https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf
 
 import pandas as pd
 import numpy as np
 
-dataSet = pd.read_csv('data/iris.csv')
+df1 = ["sepalLength","sepalWidth", "petalLength", "petalWidth","class"]
+df2 = pd.read_csv('data/iris.csv')
+
+# does this need a reference to https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf?
+dataSet = pd.concat([df1, df2])
 
 print(dataSet)
 print(np.mean(dataSet, axis=0))
