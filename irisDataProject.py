3228cfa21c71378e1e360a7122fdde730f5584ef
Declan Barr <deckybarr@googlemail.com>
2018-04-01 00:19:57 +0100

    Added "index_col=False"

--- irisDataProject.py
+++ irisDataProject.py
@@ -8,11 +8,14 @@
 import pandas as pd
 import numpy as np
 
-df1 = ["sepalLength","sepalWidth", "petalLength", "petalWidth","class"]
-df2 = pd.read_csv('data/iris.csv')
+# df1 = ['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'class']
+#df2 = pd.read_csv('data/iris.csv')
 
 # does this need a reference to https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf?
-dataSet = pd.concat([df1, df2])
+# dataSet = pd.concat([df1, df2])
+# https://stackoverflow.com/questions/33763963/pandas-data-frame-headers-are-shifted-over-when-perfoming-csv-read?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+dataSet = pd.read_csv('data/iris.csv', index_col=False )
+
 
 print(dataSet)
 print(np.mean(dataSet, axis=0))
