73c6840d919284c7d23215672af92f3d87fe92f9
Declan Barr <deckybarr@googlemail.com>
2018-03-31 23:09:45 +0100

    Added pandas library call

--- irisDataProject.py
+++ irisDataProject.py
@@ -2,8 +2,12 @@
 # Iris Flower Data Set Project for Programming and Scripting
 # Using numpy to read csv files and determine mean
 # https://www.dataquest.io/blog/numpy-cheat-sheet/
+# Using pandas to read csv files https://www.kaggle.com/ashokdavas/iris-data-analysis-pandas-numpy
 
+import pandas as pd
 import numpy as np
-dataSet = np.genfromtxt('data/iris.csv',delimiter=',')
 
+dataSet = pd.read_csv('data/iris.csv')
+
+print(dataSet)
 print(np.mean(dataSet, axis=0))
