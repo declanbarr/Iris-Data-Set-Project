fcb4b65d43ebfe7802608a59e8297d51b8b6b488
Declan Barr <deckybarr@googlemail.com>
2018-04-02 11:17:38 +0100

    Added statement to print rows 1 to 50 of the sepal length column

--- irisDataProject.py
+++ irisDataProject.py
@@ -13,4 +13,4 @@ df = pd.read_csv('data/iris.csv', names=["Sepal Length", "Sepal Width", "Petal L
 print(df)
 print(np.mean(df, axis=0)) # Axis = 0 means the columns, Axis = 1 means the rows
 print("Rows 1 to 50 are:", df.iloc[0:50])
-
+print("Rows 1 to 50 of Sepal length are:", df.iloc[0:50,[0]])
