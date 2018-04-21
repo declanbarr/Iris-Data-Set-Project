0068b116c6a3929cc568a94ffdd9b2fdc599d5c9
Declan Barr <deckybarr@googlemail.com>
2018-04-21 16:50:28 +0100

    Added option to display all rows

--- irisDataProject.py
+++ irisDataProject.py
@@ -8,6 +8,7 @@
 
 import pandas as pd
 
+pd.options.display.max_rows = 999 # Set max rows to 999 to ensure all rows are displayed (from https://pandas.pydata.org/pandas-docs/stable/options.html)
 df = pd.read_csv('data/iris.csv', names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"] ) # Read csv file and assigns headers to columns
 
 print(df.head(5)) # Prints first 5 rows
